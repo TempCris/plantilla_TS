a1d6859e65eca75cbc4308eb5c6e0680e34ce82b
Criss <93540561+TempCris@users.noreply.github.com>
2022-02-21 15:19:29 -0600

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,6 @@
-/* eslint-disable no-return-assign */
-
-// Source: https://www.hackerrank.com/challenges/simple-array-sum/problem?isFullScreen=true
-
-function simpleArraySum(ar: Array<number>): number {
-  let suma = 0;
-  ar.forEach((element) => suma += element);
-  return suma;
-}
 
 function main() {
-  console.log('---------- Cool Programing ----------\n');
-  const testArray = [1, 2, 3, 4, 10, 11];
-  const result = simpleArraySum(testArray);
-  console.log('Result: ', result);
+
 }
 
 main();
